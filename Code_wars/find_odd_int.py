988b7260d0ee13563736012ded39fb35baa9dead
Jesus Galindo <jesusgagulsa@gmail.com>
2020-10-29 17:12:54 -0600

    Cambios en formato

--- Code_wars/find_odd_int.py
+++ Code_wars/find_odd_int.py
@@ -4,8 +4,31 @@
 
 # FUNDAMENTALS
 
+def find_it(numbers):
+    b = False
+    for i in numbers:
+        odd_counter = 0
+        for j in numbers:
+            if b == True:
+                pass
+            else:
+                if i == j:
+                    odd_counter += 1
+                    aux = i
+        if odd_counter % 2 != 0:
+            b = True
+            print('The number {number} appeared an od number of times: {times}'.format(
+                number=aux, times=odd_counter))
+    if b:
+        pass
+    else:
+        print('We couldnt find a number that appaeared an odd number of times')
+        # return None
+
+
 def run():
-    pass
+    numbers = [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]
+    find_it(numbers)
 
 
 if __name__ == "__main__":
